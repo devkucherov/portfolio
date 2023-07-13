84d7a987a8609b9592eda40835f92110118b0cd0
Pasha Kucherov <dev.kucherov@gmail.com>
2023-07-13 21:26:03 +0300

    перенести стили шапки на scss

--- js/app.js
+++ js/app.js
@@ -1,41 +1,8 @@
-
-window.addEventListener('resize', function () {
-    addRequiredClass();
-})
-
-
-function addRequiredClass() {
-    if (window.innerWidth < 764) {
-        document.body.classList.add('mobile')
-    } else {
-        document.body.classList.remove('mobile')
-    }
-}
-
-window.onload = addRequiredClass
-
-let hamburger = document.querySelector('.hamburger')
-let mobileNav = document.querySelector('.nav-list')
-
-let bars = document.querySelectorAll('.hamburger span')
-
-let body = document.querySelector('body')
-
-let isActive = false
+let hamburger = document.querySelector('.header__btn')
+let mobileNav = document.querySelector('.header__menu')
+let mobileBtn = document.querySelector('.header__btn')
 
 hamburger.addEventListener('click', function () {
     mobileNav.classList.toggle('open')
-    if (!isActive) {
-        bars[0].style.transform = 'rotate(45deg)'
-        bars[1].style.opacity = '0'
-        bars[2].style.transform = 'rotate(-45deg)'
-        isActive = true
-    } else {
-        bars[0].style.transform = 'rotate(0deg)'
-        bars[1].style.opacity = '1'
-        bars[2].style.transform = 'rotate(0deg)'
-        isActive = false
-    }
-    body.classList.toggle('stopscroll')
+    mobileBtn.classList.toggle('open')
 })
-
