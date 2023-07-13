0b5af329bd79017e4ddae325ab9196f651e5ae93
Pasha Kucherov <dev.kucherov@gmail.com>
2023-07-13 21:36:14 +0300

    поправить размер шрифта меню

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 let hamburger = document.querySelector('.header__btn')
 let mobileNav = document.querySelector('.header__menu')
 let mobileBtn = document.querySelector('.header__btn')
+let body = document.querySelector('.page__body')
 
 hamburger.addEventListener('click', function () {
     mobileNav.classList.toggle('open')
     mobileBtn.classList.toggle('open')
+    body.classList.toggle('stopscroll')
 })
